e230de1af8cd59b55caaebe05c8b9ff62a8d350d
WHUSOBER <whusober@gmail.com>
2020-09-16 22:49:51 +0800

    update details

--- grayscale/main.py
+++ grayscale/main.py
@@ -4,9 +4,9 @@ import numpy as np
 img = Image.open('image.jpg')
 img_array = np.array(img)
 print(img_array.shape)
-img.show()
+# img.show()
 
 L = img.convert('L')
 L_array = np.array(L)
 print(L_array.shape)
-L.show()
+# L.show()
