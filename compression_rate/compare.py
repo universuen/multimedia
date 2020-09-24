97ee5f7468d16834b32dfd5b0b81b66e40f67109
WHUSOBER <whusober@gmail.com>
2020-09-24 09:35:41 +0800

    add decorator

--- compression_rate/compare.py
+++ compression_rate/compare.py
@@ -3,25 +3,26 @@ import cv2
 import matplotlib.pyplot as plt
 import os
 
+def calculate_duration(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.perf_counter()  # 函数开始时间
+        func(*args, **kwargs)
+        end_time = time.perf_counter()  # 函数结束时间
+        duration = end_time - start_time
+        return duration
+    return wrapper
 
+@calculate_duration
 def compress(img_path, rate):
     """
     :param img_path: 图片存放路径
     :param rate: 压缩率
     :return: 压缩用时
     """
-    start_time = time.perf_counter() # 压缩开始时间
 
     quality = int(100*(1-rate)) # 设置图片质量
     src_img = cv2.imread(img_path) # 读取图片
     cv2.imwrite("{}_{}".format(rate, img_path), src_img, [int(cv2.IMWRITE_JPEG_QUALITY), quality]) # 保存压缩后的图片
-
-    end_time = time.perf_counter() # 压缩结束时间
-
-    duration = end_time - start_time
-    # print("rate = {}, cost time = {}".format(rate, duration))
-    return duration
-
 
 if __name__ == '__main__':
     # 设置x轴和y轴
